55b2f812d684e2be986a7cbe0f409273befee782
masete <nicholasmasete72@gmail.com>
2022-08-19 09:53:11 +0300

    styling: added tailwind css

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}

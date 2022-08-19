44ba85ac14c2bf0fbb46cad04ae80f71f8062ee5
masete <nicholasmasete72@gmail.com>
2022-08-19 11:42:16 +0300

    chore: use vite instead of svelte config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
-  import "../app.css";
-</script>
-
-<slot />
+    import "../app.css";
+  </script>
+  
+  <div class="container mx-auto my-10 max-w-lg">
+    <slot></slot>
+  </div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<main>
+<h1 class="text-2xl text-center text-bold text-gray-800 md:text-3xl">To Do </h1>
+</main>

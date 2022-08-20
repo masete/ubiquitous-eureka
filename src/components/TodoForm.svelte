18ab94ade59cbfd8d56aa3d48eae1ce9a47cc128
masete <nicholasmasete72@gmail.com>
2022-08-20 11:49:11 +0300

    feat: added todofor coponent

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,7 @@
+<form class="my-6">
+    <div class="flex flex-col text-sm mb-2">
+        <label for="todo">Todo</label>
+        <input type="text" name="todo" placeholder="watcha gotta do?" />
+    </div>
+    <button type="submit" class="">Submit</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+  import TodoForm from '../components/TodoForm.svelte';
+</script>
+<main>
+  <h1 class="text-2xl text-center text-bold text-gray-800 md:text-3xl">My ToDo </h1>
+  <TodoForm/>
+</main>

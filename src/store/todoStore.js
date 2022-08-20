0b235f19b809f41a187c2b524775a9dbd5678457
masete <nicholasmasete72@gmail.com>
2022-08-20 16:08:20 +0300

    feat: added toggoled

--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -14,5 +14,23 @@ export const addTodo = (text) => {
 // @ts-ignore
 export const deleteTodo = (id) => {
     // @ts-ignore
-    todos.update(todos=>todos.filter(todos => todos.id != id))
+    todos.update(todos=>todos.filter(todos => todos.id !== id))
+}
+
+export const toggleTodoCompleted = () =>{
+    // @ts-ignore
+    todos.update(todos => {
+        let index = -1
+        for(let i=0; i < todos.length; i++ ){
+            // @ts-ignore
+            if(todos[i].id == id){
+                index =i;
+                break
+            }
+        }
+        if(index !==){
+            todos[index].completed =!todos[index].completed;
+        }
+        return todos
+    })
 }

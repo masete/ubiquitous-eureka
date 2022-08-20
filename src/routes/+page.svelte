e02c1e36c970cacc511c6e660f4ab352e5331449
masete <nicholasmasete72@gmail.com>
2022-08-20 16:48:37 +0300

    fix: typescript warnings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { todos } from '../store/todoStore.js';
+  import Todo from '../components/Todo.svelte';
   import TodoForm from '../components/TodoForm.svelte';
 </script>
+
 <main>
   <h1 class="text-2xl text-center text-bold text-gray-800 md:text-3xl">My ToDo </h1>
   <TodoForm/>
+  {#each $todos as todo}
+    <Todo todo={todo} Index={todo.id} />
+  {/each}
 </main>
